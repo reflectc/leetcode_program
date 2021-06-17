5de48c26a1fab6c3b2904c3eda33d4d68c6d23f8
cr <randeity@163.com>
2021-06-17 14:01:11 +0800

    fix format

--- src/counting_bits_338.py
+++ src/counting_bits_338.py
@@ -6,11 +6,10 @@
     @file: counting_bits_338.py
     @time: 2021/6/13 19:52
     @desc:
+        给定一个非负整数 num。对于 0 ≤ i ≤ num 范围中的每个数字 i ，
+        计算其二进制数中的 1 的数目并将它们作为数组返回。
 """
 
-
-# 给定一个非负整数 num。对于 0 ≤ i ≤ num 范围中的每个数字 i ，
-# 计算其二进制数中的 1 的数目并将它们作为数组返回。
 
 class Solution(object):
     def countBits(self, n):
